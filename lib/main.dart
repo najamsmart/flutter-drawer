c751f6e40df7b78710c550a80fed4081816e2d75
sajidali22 <sajidali12@outlook.com>
2023-05-30 21:52:31 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:drawer3d/drawer_3d.dart';
+import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
+
+void main() => runApp(
+      DevicePreview(
+        enabled: true,
+        builder: (context) => MyApp(), // Wrap your app
+      ),
+    );
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Guitar',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Drawer3D(),
+    );
+  }
+}
